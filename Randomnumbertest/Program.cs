e69d795f0d12328dadd02a203e828e7c06dc3c41
Sohail Nasir <jtdinc2000@gmail.com>
2019-02-13 23:31:37 +0000

    First Commit

---
+++ Randomnumbertest/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Randomnumbertest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var random = new Random();
+            var resultingArray = new int[1000];
+            for(var i = 0; i < 1000; i++)
+            {
+                resultingArray[i] = i + 1;
+            }
+
+            for(var i = 0; i < 1000; i++)
+            {
+                var firstAddress = random.Next(0, 999);
+                var secondAddress = random.Next(0, 999);
+                int temp = resultingArray[firstAddress];
+                resultingArray[firstAddress] = resultingArray[secondAddress];
+                resultingArray[secondAddress] = temp;
+            }
+        }
+
+    }
+}
